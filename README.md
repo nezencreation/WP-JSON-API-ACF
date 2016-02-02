2190c33658093777453583b7e2c25bfc1ea80dae
Nicholas E Zographos <nicholas@treetop.us>
2016-02-02 14:22:38 -0500

    update to work with the new v2 WP REST API

--- README.md
+++ README.md
@@ -2,8 +2,10 @@ WP-JSON-API-ACF
 ===============
 
 By default the Wordpress JSON API: [WP-API](http://wp-api.org/) doesn't include Advanced Custom Fields.
-This plugin fixes that. 
+This plugin fixes that.
 
-Grown from https://gist.github.com/rileypaulsen/9b4505cdd0ac88d5ef51 
+This plugin was written for the now depricated v1 of the [WP JSON API](http://wp-api.org/).  This updated version works with the new v2 [WP REST API](http://v2.wp-api.org/)
+
+Grown from https://gist.github.com/rileypaulsen/9b4505cdd0ac88d5ef51
 but a plugin is nicer: I didn't want to modify the other plugin
- 
+
--- plugin.php
+++ plugin.php
@@ -1,16 +1,18 @@
 <?php
 /**
- * Plugin Name: Add Advance Custom Fields to JSON API
- * Description: Add Advance Custom Fields to JSON API - from https://gist.github.com/rileypaulsen/9b4505cdd0ac88d5ef51
+ * Plugin Name: Add Advance Custom Fields to WP REST API v2
+ * Description: Add Advance Custom Fields to WP REST API v2 - from https://gist.github.com/rileypaulsen/9b4505cdd0ac88d5ef51
  * Author: @PanMan
- * Author URI: https://github.com/PanManAms/WP-JSON-API-ACF
- * Version: 0.1
- * Plugin URI: https://github.com/PanManAms/WP-JSON-API-ACF
+ * Author: @nzographos
+ * Original Author URI: https://github.com/PanManAms/WP-JSON-API-ACF
+ * v2 Author URI: https://github.com/nezencreation/WP-JSON-API-ACF
+ * Version: 0.2
+ * Plugin URI: https://github.com/nezencreation/WP-JSON-API-ACF
  * Copied from https://gist.github.com/rileypaulsen/9b4505cdd0ac88d5ef51 - but a plugin is nicer
  */
 function wp_api_encode_acf($data,$post,$context){
-    $customMeta = (array) get_fields($post['ID']);
-    $data['meta'] = array_merge($data['meta'], $customMeta );
+    $customMeta = (array) get_fields($post->ID);
+    $data->data['meta'] = $customMeta;
     return $data;
 }
 
@@ -21,13 +23,15 @@ function wp_api_encode_acf_taxonomy($data,$post){
 }
 
 function wp_api_encode_acf_user($data,$post){
-    $customMeta = (array) get_fields("user_". $data['ID']);
-    $data['meta'] = array_merge($data['meta'], $customMeta );
+    $customMeta = (array) get_fields("user_". $data->data['id']);
+    $data->data['meta'] = $customMeta;
     return $data;
 }
 
-add_filter('json_prepare_post', 'wp_api_encode_acf', 10, 3);
-add_filter('json_prepare_page', 'wp_api_encode_acf', 10, 3);
-add_filter('json_prepare_attachment', 'wp_api_encode_acf', 10, 3);
-add_filter('json_prepare_term', 'wp_api_encode_acf_taxonomy', 10, 2);
-add_filter('json_prepare_user', 'wp_api_encode_acf_user', 10, 2);
+add_filter('rest_prepare_post', 'wp_api_encode_acf', 10, 3);
+add_filter('rest_prepare_page', 'wp_api_encode_acf', 10, 3);
+add_filter('rest_prepare_attachment', 'wp_api_encode_acf', 10, 3);
+add_filter('rest_prepare_term', 'wp_api_encode_acf_taxonomy', 10, 2);
+add_filter('rest_prepare_user', 'wp_api_encode_acf_user', 10, 2);
+add_filter('rest_prepare_user', 'wp_api_encode_acf_user', 10, 2);
+ // add_filter('rest_prepare_<your custom post type slug>', 'wp_api_encode_acf', 10, 3);
